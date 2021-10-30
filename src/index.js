7c0adeb9e3c1c0f44d4a8aedbb353f929aae099c
Bernard <bernard@vdwe.es>
2021-10-30 17:57:28 +0200

    Add next, pref buttons

--- src/index.js
+++ src/index.js
@@ -4,25 +4,34 @@ const f = "/assets/F.png";
 const url = "http://img.itadvice.de/fbar/";
 
 const images = [
-  "IMG_20211017_165941_00_merged.jpg",
-  "IMG_20211017_170211_00_merged.jpg",
-  "IMG_20211017_170304_00_merged.jpg",
-  "IMG_20211017_170339_00_merged.jpg",
-  "IMG_20211017_170425_00_merged.jpg",
-  "IMG_20211017_170455_00_merged.jpg",
-  "IMG_20211017_170537_00_merged.jpg",
-  "IMG_20211017_170617_00_merged.jpg",
-  "IMG_20211017_170711_00_merged.jpg",
-  "IMG_20211017_170806_00_merged.jpg",
-  "IMG_20211017_170913_00_merged.jpg",
+  "IMG_20211017_170537_00_merged.jpg", // outside
+  "IMG_20211017_170455_00_merged.jpg", // front sofa
+  "IMG_20211017_165941_00_merged.jpg", // bar
+  "IMG_20211017_170425_00_merged.jpg", // bar behind till
+  "IMG_20211017_170339_00_merged.jpg", // back 1 front
+  "IMG_20211017_170211_00_merged.jpg", // back 2 middle
+  "IMG_20211017_170304_00_merged.jpg", // back 3 back
+  "IMG_20211017_170617_00_merged.jpg", // hall 1
+  "IMG_20211017_170711_00_merged.jpg", // hall 2
+  "IMG_20211017_170806_00_merged.jpg", // hall 3
+  "IMG_20211017_170913_00_merged.jpg", // hall 4
 ];
 
-//const panorama = new PANOLENS.ImagePanorama(`${url}${images[0]}`);
 const viewer = new PANOLENS.Viewer();
 
-const panoramas = [];
+let currentPanorama = 0;
+window.next = function () {
+  if (currentPanorama !== 0) {
+    viewer.setPanorama(panoramas[currentPanorama - 1]);
+  }
+}
+window.prev = function () {
+  if (currentPanorama < panoramas.length) {
+    viewer.setPanorama(panoramas[currentPanorama + 1]);
+  }
+}
 
-
+let panoramas = [];
 
 // Create panoramas from all images
 images.forEach((image, key) => {
@@ -30,22 +39,10 @@ images.forEach((image, key) => {
   viewer.add(panoramas[key]);
 
   // Directly pair panorama to previous
-  if (key !== 0) {
+  if (key > 0) {
     // THREE.Vector3( X, Y, Z )
-    panoramas[key - 1].link(panoramas[key], new THREE.Vector3(-10, -30, -80), 8, arrow);//PANOLENS.DataImage.Arrow);
-    panoramas[key].link(panoramas[key - 1], new THREE.Vector3(-10, -30, 80), 8, f);//PANOLENS.DataImage.Arrow);
-    console.log(key, key - 1, 'linked');
-  }
-  if (key !== images.length) {
+    panoramas[key - 1].link(panoramas[key], new THREE.Vector3(-10, -30, -80), 8, arrow);
+    panoramas[key].link(panoramas[key - 1], new THREE.Vector3(-10, -30, 80), 8, f);
   }
 
 });
-console.log(PANOLENS.DataImage.Arrow);
-
-// panoramas.forEach
-
-// panorama1.link( panorama2, new THREE.Vector3( -3145.23, -3704.40, 1149.48 ) );
-// panorama2.link( panorama1, new THREE.Vector3( -3429.01, 1205.85, -3421.88 ) );
-
-
-console.log(panoramas);

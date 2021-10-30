a346b89d4c05bf449ba11a8a839bee8172219745
Bernard <bernard@vdwe.es>
2021-10-30 11:40:40 +0200

    Version somewhat works but needs transpiling

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
-import * as THREE from './node_modules/three/build/three.min.js';
-import * as PANOLENS from "./node_modules/panolens/build/panolens.min.js";
+// import * as THREE from './node_modules/three/build/three.min.js';
+// import * as PANOLENS from "./node_modules/panolens/build/panolens.min.js";
 
-const panorama = new PANOLENS.ImagePanorama( 'IMG_20211017_165941_00_merged.jpg' );
+// import THREE from './node_modules/three/build/three.module.js';
+import * as three from './node_modules/three/build/three.module.js';
+import * as Panolens from './node_modules/panolens/build/panolens.module.js';
+
+
+
+let TWEEN    = Panolens.TWEEN;
+let PANOLENS = Panolens.PANOLENS;
+
+// console.log(panolens, three, PANOLENS);
+
+const panorama = new PANOLENS.ImagePanorama( 'asset/equirectangular.jpg' );
 const viewer = new PANOLENS.Viewer();
 viewer.add( panorama );

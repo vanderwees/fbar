85fa4fae9acf818c37c20ad4928caea4d4c5e670
Bernard <bernard@vdwe.es>
2021-10-30 15:08:46 +0200

    Add images urls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+const url = "http://img.itadvice.de/fbar/";
 
-const panorama = new PANOLENS.ImagePanorama( 'assets/001.jpg' );
+const images = [
+  "IMG_20211017_165941_00_merged.jpg",
+  "IMG_20211017_170211_00_merged.jpg",
+  "IMG_20211017_170304_00_merged.jpg",
+  "IMG_20211017_170339_00_merged.jpg",
+  "IMG_20211017_170425_00_merged.jpg",
+  "IMG_20211017_170455_00_merged.jpg",
+  "IMG_20211017_170537_00_merged.jpg",
+  "IMG_20211017_170617_00_merged.jpg",
+  "IMG_20211017_170711_00_merged.jpg",
+  "IMG_20211017_170806_00_merged.jpg",
+  "IMG_20211017_170913_00_merged.jpg",
+];
+
+const panorama = new PANOLENS.ImagePanorama(`${url}${images[0]}`);
 const viewer = new PANOLENS.Viewer();
-viewer.add( panorama );
+viewer.add(panorama);

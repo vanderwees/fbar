da13e93adfc6e039712a00cc54e2163fb2264b4c
Bernard <bernard@vdwe.es>
2021-10-30 11:59:23 +0200

    Again not working import

--- index.js
+++ index.js
@@ -3,15 +3,20 @@
 // import * as PANOLENS from "./node_modules/panolens/build/panolens.min.js";
 
 // import THREE from './node_modules/three/build/three.module.js';
-import * as three from './node_modules/three/build/three.module.js';
-import * as Panolens from './node_modules/panolens/build/panolens.module.js';
+// import * as three from './node_modules/three/build/three.module.js';
+// import * as Panolens from './node_modules/panolens/build/panolens.module.js';
+// import * as three from './lib/three.min.js';
+// import Panolens from './lib/panolens.min.js';
 
 
 
-let TWEEN    = Panolens.TWEEN;
-let PANOLENS = Panolens.PANOLENS;
+// let TWEEN    = Panolens.TWEEN;
+// let PANOLENS = Panolens.PANOLENS;
 
-// console.log(panolens, three, PANOLENS);
+import PANOLENS  from "./node_modules/panolens-three/dist/index.js";
+const THREE =  PANOLENS.THREE;
+
+console.log(THREE, PANOLENS);
 
 const panorama = new PANOLENS.ImagePanorama( 'asset/equirectangular.jpg' );
 const viewer = new PANOLENS.Viewer();
